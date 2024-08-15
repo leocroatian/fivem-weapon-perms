0bcf385877f3f42addc5b2d616751bb8ebac12e0
leocroatian <104053610+leocroatian@users.noreply.github.com>
2024-08-15 18:25:47 -0400

    fixed perms

--- server.lua
+++ server.lua
@@ -3,25 +3,29 @@ AddEventHandler('WeaponPerms:CheckPerms', function(weapon)
 
     local result = {}
 
-    if IsPlayerAceAllowed(source, Weapons.BypassPermission) then
-        result = {
-            allowed = true,
-            hash = weapon
-        }
-        TriggerClientEvent('WeaponPerms:Result', source, result)
-        return
-    elseif Weapons.weps[weapon] then
-        result = {
-            allowed = IsPlayerAceAllowed(source, Weapons.weps[weapon].ace),
-            hash = weapon
-        }
-        TriggerClientEvent('WeaponPerms:Result', source, result)
-        return
-    else
-        result = {
-            allowed = Weapons.AllowedByDefault
-        }
-        TriggerClientEvent('WeaponPerms:Result', source, result)
-        return
+    local unarmed = GetHashKey('weapon_unarmed')
+
+    if weapon ~= unarmed then
+        if IsPlayerAceAllowed(source, Weapons.BypassPermission) then
+            result = {
+                allowed = true,
+                hash = weapon
+            }
+            TriggerClientEvent('WeaponPerms:Result', source, result)
+            return
+        elseif Weapons.weps[weapon] then
+            result = {
+                allowed = IsPlayerAceAllowed(source, Weapons.weps[weapon].ace),
+                hash = weapon
+            }
+            TriggerClientEvent('WeaponPerms:Result', source, result)
+            return
+        else
+            result = {
+                allowed = Weapons.AllowedByDefault
+            }
+            TriggerClientEvent('WeaponPerms:Result', source, result)
+            return
+        end
     end
 end)
